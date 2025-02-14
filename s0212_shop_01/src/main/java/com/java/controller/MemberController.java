147a8eabbed25ccbed6cce50fcdedbbfc36b870e
vhfflxbzmffpdhxkdlem <wonwow55@naver.com>
2025-02-14 13:02:38 +0900

    2014

---
+++ s0212_shop_01/src/main/java/com/java/controller/MemberController.java
@@ -0,0 +1,38 @@
+package com.java.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.java.dto.MemberDto;
+import com.java.service.MemberService;
+
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+public class MemberController {
+	
+	@Autowired HttpSession session;
+	@Autowired MemberService memberService;
+	
+	@GetMapping("/member/login")
+	public String login() {
+		return "member/login";
+	}
+	
+	@PostMapping("/member/login")
+	public String login(MemberDto mdto, Model model) {
+		System.out.println("controller id : "+mdto.getId());
+		MemberDto memberDto = memberService.login(mdto);
+		if(memberDto!=null) {
+			session.setAttribute("session_id", memberDto.getId());
+			return "redirect:/?loginChk=1";
+		}else {
+			model.addAttribute("loginChk",0);
+			return "member/login";
+		}
+	}
+
+}
